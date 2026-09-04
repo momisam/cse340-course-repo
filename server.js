189dc2180f8b9d629351b3db4d5ca69e5572a6a3
Moses Samuel <mosesfemi10@gmail.com>
2026-09-04 13:16:50 +0100

    feat: serve static files from public directory

--- server.js
+++ server.js
@@ -16,6 +16,15 @@ const __dirname = path.dirname(__filename);
 // Create an instance of the Express
 const app = express();
 
+/**
+  * Configure Express middleware
+  */
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
 app.get("/", (req, res) => {
   res.send("Hello from Express! Block 5");
 });
