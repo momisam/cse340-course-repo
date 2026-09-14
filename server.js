54db1e106c6e08608f1f1fde6726a3fa274c28a2
Moses Samuel <mosesfemi10@gmail.com>
2026-09-14 18:23:37 +0100

    feat: update projects view to dynamically display project data

--- server.js
+++ server.js
@@ -49,14 +49,13 @@ app.get('/', async (req, res) => {
 
 app.get('/organizations', async (req, res) => {
     const organizations = await getAllOrganizations();
-     const title = "Our Partner Organizations";
+    const title = "Our Partner Organizations";
 
     res.render('organizations', { title, organizations });
 });
 
 app.get('/projects',  async (req, res) => {
     const projects = await getAllProjects();
-    console.log(projects);
 
     const title = "Service Projects";
     res.render('projects', { title, projects });
